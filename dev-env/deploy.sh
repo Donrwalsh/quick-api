c591ab31f92b34ae09db426c4e1741e5ba5f9c56
Don R Walsh <donrwalsh@gmail.com>
2018-06-14 16:18:22 -0500

    Deploy script update
    This small change lets you deploy the script without building it and running tests. Important because once there are tests, the previous deploy script would fail if the tests fail; not great.

--- dev-env/deploy.sh
+++ dev-env/deploy.sh
@@ -2,7 +2,7 @@
 rm -rfv dev-box/*
 mkdir -p dev-box/frontend
 cp ../frontend/* dev-box/frontend/ -r
-mvn package -f ../api/employees/pom.xml
+mvn package -f ../api/employees/pom.xml -Dmaven.test.skip=true
 cp ../api/employees/target/employees-0.0.1-SNAPSHOT.war.original dev-box/employees.war
 vagrant ssh -c "sudo rm -rfv /var/lib/tomcat8/webapps/ROOT/*;
 				sudo cp -rv /scratch/frontend/* /var/lib/tomcat8/webapps/ROOT/;
