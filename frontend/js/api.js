f13c7d7d4efaa4688caf38116fd508e891f2b173
Don R Walsh <donrwalsh@gmail.com>
2018-06-18 07:10:13 -0500

    Create form output
    Simple display of response from the create endpoint.

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -16,7 +16,29 @@ $(function () {
             url: "/employees/employees/",
             data: JSON.stringify(formData),
             processData: false,
-            contentType: "application/json"
+            contentType: "application/json",
+            success: function(res, status, xhr) {
+                if (xhr.status === 201) {
+                    $.each(res, function (i, response) {
+                       let resJSON = "";
+                       if (response === "") {
+                           resJSON = "{Empty Response}";
+                       } else {
+                           resJSON += "<p>";
+                           resJSON += "<strong>Status:</strong> " + xhr.status + "<br />";
+                           resJSON += "<strong>First Name:</strong> " + response.firstName + "<br />";
+                           resJSON += "<strong>Last Name:</strong> " + response.lastName + "<br />";
+                           resJSON += "<strong>Gender:</strong> " + response.gender + "<br />";
+                           resJSON += "<strong>Birth Date:</strong> " + response.birthDate + "<br />";
+                           resJSON += "<strong>Hire Date:</strong> " + response.hireDate + "<br />";
+                           resJSON += "</p>"
+                       }
+                       $(".output").append(resJSON);
+                    });
+                } else {
+                    alert("failure");
+                }
+            }
         });
     })
 });
