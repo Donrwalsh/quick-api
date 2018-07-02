cc0ad66804b8240303591ab88867f15b0dfa5d99
Don R Walsh <donrwalsh@gmail.com>
2018-07-02 16:22:16 -0500

    Strong Verification
    Almost guaranteed that the reason things were breaking before was due to this Annotation missing. Without it, things were working thanks to previous reports missing. This should now create/overwrite this report each and every time. Now let's see what Jenkins does with it.
    
    (This verification came about from Jenkins experiencing the error that I thought I'd solved, pretty obvious after that)

--- api/test/src/test/java/feature/JDBC/JDBCTest.java
+++ api/test/src/test/java/feature/JDBC/JDBCTest.java
@@ -4,6 +4,7 @@ import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
+@CucumberOptions(format = {"json:target/surefire-reports/jdbc-test.json"})
 @RunWith(Karate.class)
 public class JDBCTest {
 }
