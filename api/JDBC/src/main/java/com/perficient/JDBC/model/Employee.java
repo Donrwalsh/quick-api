5334a2135aa9bb4015bbbfec88ccd633799aea87
Don R Walsh <donrwalsh@gmail.com>
2018-07-04 11:38:03 -0500

    Expanded Test Suite
    Pulled in the Test Suite from Integration Branch with the associated DateTime fix (JDBC Only)

--- api/JDBC/src/main/java/com/perficient/JDBC/model/Employee.java
+++ api/JDBC/src/main/java/com/perficient/JDBC/model/Employee.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class Employee {
 
     private int emp_no;
     @NotNull
-    private Date birth_date;
+    private LocalDate birth_date;
     @NotNull
     @NotEmpty
     private String first_name;
@@ -21,9 +22,9 @@ public class Employee {
     @NotEmpty
     private String gender;
     @NotNull
-    private Date hire_date;
+    private LocalDate hire_date;
 
-    public Employee(int emp_no, Date birth_date, String first_name, String last_name, String gender, Date hire_date) {
+    public Employee(int emp_no, LocalDate birth_date, String first_name, String last_name, String gender, LocalDate hire_date) {
         this.emp_no = emp_no;
         this.birth_date = birth_date;
         this.first_name = first_name;
@@ -35,7 +36,7 @@ public class Employee {
     public Employee() {}
 
     @JsonCreator
-    public Employee(Date birth_date, String first_name, String last_name, String gender, Date hire_date) {
+    public Employee(LocalDate birth_date, String first_name, String last_name, String gender, LocalDate hire_date) {
         this.birth_date = birth_date;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -44,11 +45,11 @@ public class Employee {
     }
 
     public int getEmpNo() { return emp_no; }
-    public Date getBirthDate() { return birth_date; }
+    public LocalDate getBirthDate() { return birth_date; }
     public String getFirstName() { return first_name; }
     public String getLastName() { return last_name; }
     public String getGender() { return gender; }
-    public Date getHireDate() { return hire_date; }
+    public LocalDate getHireDate() { return hire_date; }
 
     public void setEmp_no(int emp_no) {
         this.emp_no = emp_no;
--- api/test/src/test/resources/feature/JDBC/JDBC.feature
+++ api/test/src/test/resources/feature/JDBC/JDBC.feature
@@ -1,7 +1,43 @@
-Feature: CRUD
+Feature: Developer Environment Employee CRUD Endpoints
 
-  Scenario: Read from database
+  Background:
+    * def URL = apiURL +  '/JDBC/employees/'
 
-    Given url apiURL + '/JDBC/employees/12345'
+  Scenario: Simple JDBC CRUD Validation
+
+    #Create
+    Given url URL
+    And request {"gender":"M","birth_date":"1987-10-22","last_name":"Tables","hire_date":"2001-01-01","first_name":"Robert"}
+    When method POST
+    Then status 201
+    * def id = response[0].empNo
+    And match response[0] contains {gender:'M', birthDate:'1987-10-22', firstName:'Robert', lastName:'Tables', hireDate:'2001-01-01', empNo:'#notnull'}
+
+    #Validate (Read)
+    Given url URL + id
     When method GET
     Then status 200
+    And match response[0] contains {gender:'M', birthDate:'1987-10-22', firstName:'Robert', lastName:'Tables', hireDate:'2001-01-01', empNo:'#(id)'}
+
+    #Update
+    Given url URL + id
+    And request {"gender":"F","birth_date":"1990-08-07","last_name":"Farvour","hire_date":"2001-01-01","first_name":"Laura"}
+    When method PUT
+    Then status 200
+    And match response[0] contains {"gender":"F","birthDate":"1990-08-07","lastName":"Farvour","hireDate":"2001-01-01","firstName":"Laura"}
+
+    #Validate (Read)
+    Given url URL + id
+    When method GET
+    Then status 200
+    And match response[0] contains {"gender":"F","birthDate":"1990-08-07","lastName":"Farvour","hireDate":"2001-01-01","firstName":"Laura"}
+
+    #Delete
+    Given url URL + id
+    When method DELETE
+    Then status 204
+
+    #Validate (Read)
+    Given url URL + id
+    When method GET
+    Then status 404
